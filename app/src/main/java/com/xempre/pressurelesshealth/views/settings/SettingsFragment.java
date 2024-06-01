4063854ec2cb80da695e8b94f1d66f62d2cce702
Leonardo Molina <dev@xempre.com>
2024-06-01 01:39:25 -0500

    Feature: Add confirm popup to logout.

--- app/src/main/java/com/xempre/pressurelesshealth/views/settings/SettingsFragment.java
+++ app/src/main/java/com/xempre/pressurelesshealth/views/settings/SettingsFragment.java
@@ -1,6 +1,8 @@
 package com.xempre.pressurelesshealth.views.settings;
 
 import android.app.AlarmManager;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
@@ -142,16 +144,31 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             @Override
             public boolean onPreferenceClick(Preference preference) {
                 // Realiza alguna acción al hacer clic en el botón
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setMessage("¿Está seguro de cerrar sesión?")
+                        .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                SharedPreferences sharedPreferences = getContext().getSharedPreferences("MySharedPref", getContext().MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.remove("token");
+                                editor.remove("userId");
+                                editor.apply();
 
-                SharedPreferences sharedPreferences = getContext().getSharedPreferences("MySharedPref", getContext().MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.remove("token");
-                editor.remove("userId");
-                editor.apply();
+                                Intent intent = new Intent(getActivity(), MainActivity.class);
+                                startActivity(intent);
+                                mainActivity.finish();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // Cancelar la eliminación
+                                dialog.dismiss();
+                            }
+                        });
+                // Crear y mostrar el diálogo
+                AlertDialog dialog = builder.create();
+                dialog.show();
 
-                Intent intent = new Intent(getActivity(), MainActivity.class);
-                startActivity(intent);
-                getActivity().finish();
                 return true;
             }
         });
